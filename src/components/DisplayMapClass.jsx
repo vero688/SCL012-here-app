bad5ecfc9e16b13bcfd1b76a5840393312b20af8
tamaramunoz <tami.skt@gmail.com>
2020-03-23 12:52:12 -0300

    navbar y vistas vero

--- src/components/DisplayMapClass.jsx
+++ src/components/DisplayMapClass.jsx
@@ -1,47 +1,47 @@
-import React from 'react';
+// import React from 'react';
 
-export default class DisplayMapClass extends React.Component {
-    mapRef = React.createRef();
-    state = {
-      map: null
-    };
+// export default class DisplayMapClass extends React.Component {
+//     mapRef = React.createRef();
+//     state = {
+//       map: null
+//     };
   
-    componentDidMount() {
-      const H = window.H;
-      const platform = new H.service.Platform({
-          apikey: "{D0q9uku4PfaL9gJtiIsF-OzqBPCPCHguQqSupgbhMhY}"
-      });
+//     componentDidMount() {
+//       const H = window.H;
+//       const platform = new H.service.Platform({
+//           apikey: "{D0q9uku4PfaL9gJtiIsF-OzqBPCPCHguQqSupgbhMhY}"
+//       });
   
-      const defaultLayers = platform.createDefaultLayers();
+//       const defaultLayers = platform.createDefaultLayers();
   
-      const map = new H.Map(
-        this.mapRef.current,
-        defaultLayers.vector.normal.map,
-        {
-          center: { lat: 50, lng: 5 },
-          zoom: 4,
-          pixelRatio: window.devicePixelRatio || 1
-        }
-      );
+//       const map = new H.Map(
+//         this.mapRef.current,
+//         defaultLayers.vector.normal.map,
+//         {
+//           center: { lat: 50, lng: 5 },
+//           zoom: 4,
+//           pixelRatio: window.devicePixelRatio || 1
+//         }
+//       );
   
-      // MapEvents enables the event system
-      // Behavior implements default interactions for pan/zoom (also on mobile touch environments)
-      // This variable is unused and is present for explanatory purposes
-      // const behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+//       // MapEvents enables the event system
+//       // Behavior implements default interactions for pan/zoom (also on mobile touch environments)
+//       // This variable is unused and is present for explanatory purposes
+//       // const behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
   
-      // Create the default UI components to allow the user to interact with them
-      // This variable is unused
-      // const ui = H.ui.UI.createDefault(map, defaultLayers);
+//       // Create the default UI components to allow the user to interact with them
+//       // This variable is unused
+//       // const ui = H.ui.UI.createDefault(map, defaultLayers);
   
-      this.setState({ map });
-    }
+//       this.setState({ map });
+//     }
   
-    componentWillUnmount() {
-      this.state.map.dispose();
-    }
+//     componentWillUnmount() {
+//       this.state.map.dispose();
+//     }
   
-    render() {
-      return <div ref={this.mapRef} style={{ height: "500px" }} />;
-    }
-  }
+//     render() {
+//       return <div ref={this.mapRef} style={{ height: "500px" }} />;
+//     }
+//   }
   
