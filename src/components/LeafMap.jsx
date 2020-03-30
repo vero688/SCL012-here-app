c926a337900bc5db66b4fa6b3926322e93c068fd
tamaramunoz <tami.skt@gmail.com>
2020-03-30 11:23:59 -0300

    agregando json con lugares pet friendly

--- src/components/LeafMap.jsx
+++ src/components/LeafMap.jsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
 import MapContainer from './MapContainer'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 
 
 function LeafMap() {
-  
+
+  const [locals, setLocals] = useState([]);
+
+
   const center = [-33.4569397, -70.6482697];  // {lat, lng}
   const zoom = 16;
 
   return (
+    <Fragment>
       <div className="container-map">
-        <MapContainer 
+        <MapContainer
           center={center}
           zoom={zoom}
         />
+
+        { locals.map(item => {
+          return (
+            <Marker position={item.position}>
+              <Popup>{item.content}</Popup>
+            </Marker>
+          )
+        })
+        }
       </div>
+    </Fragment>
   );
 }
 
